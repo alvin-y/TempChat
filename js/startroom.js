33a855ad2c439ef767296e9d7da2438e392cea89
CalvinJW <calvinjohnwang@gmail.com>
2019-03-03 07:08:25 -0500

    Add files via upload

---
+++ js/startroom.js
@@ -0,0 +1,46 @@
+var x = document.getElementById("create");
+x.addEventListener("click", function(event){
+  event.preventDefault()
+});
+x.addEventListener("click", create);
+
+var y = document.getElementById("join");
+y.addEventListener("click", function(event){
+  event.preventDefault()
+});
+y.addEventListener("click", join);
+
+function error(bad){
+	bad.style.backgroundColor = "pink";
+	bad.style.borderColor = "red";
+	bad.style.backgroundImage = "url(images/error.png)";
+}
+function clean(good){
+	good.style.borderColor="";
+	good.style.backgroundColor = "white";
+	good.style.backgroundImage = "";
+}
+function create(){
+	var nickname = document.getElementById("name");
+	var room = document.getElementById("roomid");
+
+	if(nickname.value == ""){
+		error(nickname);
+	}else{
+		clean(nickname);
+	}
+	if(room.value == ""){
+		error(room);
+	}else{
+		clean(room);
+	}
+}
+function join(){
+	var nickname = document.getElementById("name");
+
+	if(nickname.value == ""){
+		error(nickname);
+	}else{
+		clean(nickname);
+	}
+}
