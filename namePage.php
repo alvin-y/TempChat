5ae9aac16bbc7ec1f11b19d119c5305f03529e73
alvin-y <alvin.yao@hotmail.ca>
2019-03-31 18:46:56 -0400

    Bootstrap and name set page
    Button to enter not yet complete

---
+++ namePage.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<html>
+	<head>
+		<title> Chat Room </title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/nameStyle.css">
+		<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<div class="vertical-center">
+			<div class="jumbotron row">
+				<div class="col-sm">
+					<h1 class="display-4 col-sm-5">TempChat</h1>
+				</div>
+				<form class="col-sm-7 row">
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Display Name">
+						<div class="input-group-append">
+							<button class="btn btn-outline-secondary" type="button">Enter</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
