97abeb57eeefab49227c752585f2210db2833401
CalvinJW <calvinjohnwang@gmail.com>
2019-04-01 19:11:09 -0400

    Add files via upload

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,20 +1,24 @@
 var sendButton = document.getElementById("send");
 var messageBox = document.getElementById("message");
+sendButton.addEventListener("click", function(event){
+  event.preventDefault()
+});
 
 sendButton.addEventListener("click", sendMessage);
 
 var countMessages = 0;
 
 function sendMessage(){
-	var node = document.createTextNode(messageBox.value);
+	var user = document.getElementById("username").value;
+	var bold = document.createElement('strong');
+	var node = document.createTextNode(user + " : " +messageBox.value);
 	var newPara = document.createElement("p");
 	newPara.appendChild(node);
-	
 	var chatBox = document.getElementById("chatBox");
+	
 	chatBox.appendChild(newPara);
-	countMessages++;
-	if(countMessages >= 15){
-		chatBox.removeChild(chatBox.childNodes[0]);
-	}
+	chatBox.scrollTop = chatBox.scrollHeight;
+
 	messageBox.value = "";
 }
+
