c966eea25ca5aa7240a94f78e8bd418cf1c4cfeb
CalvinJW <calvinjohnwang@gmail.com>
2019-04-01 19:10:30 -0400

    Add files via upload

--- chatPage.php
+++ chatPage.php
@@ -4,7 +4,7 @@
 	if(!isset($_SESSION["name"])){
 		header("Location: namePage.php");
 	}
-	
+	$user = $_SESSION["name"];
 	$roomID = $_SESSION["roomID"];
 ?>
 
@@ -18,19 +18,20 @@
 	</head>
 
 	<body>
+	<?php echo "<input type='hidden' id='username' value='$user' />"; ?>
 		<div class="mainBox">
 			<div class="IDBar" id="IDBar">
 				<h2 id="roomID">Room ID: <?php echo $roomID ?></h2>
 			</div>
 
-			<div class="chatBox" id="chatBox">
+			<div class="chatBox" id="chatBox" style="overflow-y:scroll;">
 
 			</div>
 			
-			<div class="userChat">
-				<input type="text" class="textInput" id = "message"  name="message" value="" placeholder="Message">
-				<button class="sendButton" id="send">Send</button>
-			</div>
+			<form id="chat" class="userChat" method = 'POST'>
+				<input type="text" class="textInput" id = "message"  name="message" value="" placeholder="Message" autofocus>
+				<input type="submit" class="submit" id="send"></input>
+			</form>
 		
 		</div>
 		
