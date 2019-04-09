ec23232bc04a5434aa15bf3b53ea8b8f2191239b
CalvinJW <calvinjohnwang@gmail.com>
2019-04-09 19:05:44 -0400

    Add files via upload

---
+++ roomStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+//roomStatus.php
+
+include 'functions/functions.php';
+$conn = getDB();
+
+session_start();
+
+$roomID = $_SESSION["roomID"];
+
+$statement = $conn->prepare("SELECT id FROM rooms WHERE ? = id");
+$statement->bind_param("i", $roomID);
+$statement->execute();
+$result = $statement->get_result();
+
+$a = [];
+while($row = mysqli_fetch_assoc($result)){
+	$a[] = array(utf8_encode($row["id"]));
+}
+
+echo json_encode($a);
+
+?>
