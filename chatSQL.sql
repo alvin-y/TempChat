a21b139c18f7103e0bed8a693f816554810be93e
alvin-y <alvin.yao@hotmail.ca>
2019-04-09 17:26:32 -0400

    Add files via upload

--- chatSQL.sql
+++ chatSQL.sql
@@ -12,9 +12,9 @@ USE `tempchat`;
 # Table structure for table 'Rooms'
 #
 
-DROP TABLE IF EXISTS `Rooms`;
+DROP TABLE IF EXISTS `rooms`;
 
-CREATE TABLE `Rooms` (
+CREATE TABLE `rooms` (
   `id` INTEGER NOT NULL, 
   `roomPass` VARCHAR(255) NOT NULL, 
   `userCount` int(11),  
@@ -28,9 +28,9 @@ SET autocommit=1;
 # Table structure for table 'Users'
 #
 
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `Users` (
+CREATE TABLE `users` (
   `userID` INTEGER NOT NULL AUTO_INCREMENT, 
   `name` VARCHAR(12), 
   PRIMARY KEY (`userID`) 
@@ -43,9 +43,9 @@ SET autocommit=1;
 # Table structure for table 'RoomLogs'
 #
 
-DROP TABLE IF EXISTS `RoomLogs`;
+DROP TABLE IF EXISTS `roomlogs`;
 
-CREATE TABLE `RoomLogs` (
+CREATE TABLE `roomlogs` (
   `roomID` INTEGER NOT NULL, 
   `senderID` int(11) NOT NULL, 
   `msgID` int(11) NOT NULL AUTO_INCREMENT, 
@@ -63,9 +63,9 @@ SET autocommit=1;
 # Table structure for table 'RoomUsers'
 #
 
-DROP TABLE IF EXISTS `RoomUsers`;
+DROP TABLE IF EXISTS `roomusers`;
 
-CREATE TABLE `RoomUsers` (
+CREATE TABLE `roomusers` (
   `roomID` INTEGER NOT NULL, 
   `userID` INTEGER NOT NULL,
   FOREIGN KEY (roomID) REFERENCES rooms(id),
