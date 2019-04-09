0fb0b8280fb561411a2f24f5d90b2a9578c05f54
alvin-y <alvin.yao@hotmail.ca>
2019-04-08 22:43:33 -0400

    Add files via upload

--- startPage.php
+++ startPage.php
@@ -7,13 +7,15 @@
 	if(!isset($_SESSION["name"])){
 		header("Location: namePage.php");
 	}
-
+	
+	$userID = $_SESSION["userID"];
+	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (isset($_POST['create'])) {
+		if (isset($_POST['create'])) {     //MAKE NEW ROOM
 			//create-button was clicked
 			$id = mt_rand(0,10000);
 			
-			//check if room alread exists
+			//check if room already exists
 			$statement = $conn->prepare("SELECT id FROM rooms WHERE id = ?");
 			$statement->bind_param("i", $id);
 			$statement->execute();
@@ -28,23 +30,31 @@
 			}
 			//hash the password and insert info into room
 			$newPass = password_hash(trim($_POST["roomPassMake"]), PASSWORD_DEFAULT);
-			$statement = $conn->prepare('INSERT INTO rooms (id, roomPass) VALUES (?, ?)');
-			$statement->bind_param("is", $id, $newPass);
+			
+			$statement = $conn->prepare('INSERT INTO rooms (id, roomPass, userCount) VALUES (?, ?, ?)');
+			$temp = 1;
+			$statement->bind_param("isi", $id, $newPass, $temp);
 			$statement->execute();
+			
+			$statement = $conn->prepare('INSERT INTO roomusers (roomID, userID) VALUES (?, ?)');
+			$statement->bind_param("ii", $id, $userID);
+			$statement->execute();		
 			
 			$_SESSION["roomID"] = $id;
 		
 			header("Location: chatPage.php");
 		}
-		elseif (isset($_POST['join'])) {
+		elseif (isset($_POST['join'])) {      //JOIN ROOM
 			# join-button was clicked
 			$id = $_POST["roomID"];
 			$statement = $conn->prepare("SELECT id FROM rooms WHERE id = ?");
 			$statement->bind_param("i", $id);
 			$statement->execute();
 			$result = $statement->get_result();
+			
 			if(mysqli_num_rows($result) == 0){ //room doesn't exist
 				echo '<script type="text/javascript"> alert("This room does not exist"); </script>';
+				
 			} else { //check password
 				$id = $_POST["roomID"];
 				$roomPass = $_POST["roomPassJoin"];
@@ -53,7 +63,23 @@
 				$statement->execute();
 				$result = $statement->get_result();
 				$passCheck = mysqli_fetch_assoc($result)["roomPass"];
+				
 				if(password_verify($roomPass, $passCheck)){ //if password matches
+					//check if user already exists
+					$statement = $conn->prepare("SELECT userID FROM roomusers WHERE (roomID = ? AND userID = ?)");
+					$statement->bind_param("ii", $id, $userID);
+					$statement->execute();
+					$result = $statement->get_result();	
+					echo mysqli_fetch_assoc($result)["userID"];
+					if(mysqli_num_rows($result) == 0){ //not already in room
+						$statement = $conn->prepare('INSERT INTO roomusers (roomID, userID) VALUES (?, ?)');
+						$statement->bind_param("ii", $id, $userID);
+						$statement->execute();	
+						
+						$statement = $conn->prepare('UPDATE rooms SET userCount = userCount + 1 WHERE id = ?');
+						$statement->bind_param("i", $id);
+						$statement->execute();
+					}
 					$_SESSION["roomID"] = $id;
 					header("Location: chatPage.php");
 				} else { //wrong password
