d3574620dcd1ed91dc65c82e936c91963ebd43e1
CalvinJW <calvinjohnwang@gmail.com>
2019-04-09 19:51:08 -0400

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -14,37 +14,5 @@
 		return $result;
 		// takes a reference to the DB and a query and returns the results of running the query on the database
 	}	
-	
-	function fetch_user_chat($room, $user, $conn){
-		$query = "
-		SELECT * FROM roomlogs
-		WHERE (from roomID = '".$room."'
-		AND senderID = '".$user."')
-		ORDER BY msgID ASC
-		";
-		$statement = $conn->prepare($query);
-		$statement->execute();
-		$result = $statement->fetchAll();
-		$output = '<ul class="list-unstyled">';
-		foreach($result as $row)
-		{
-			$username = '';
-			if($row["senderID"] == $user)
-				$username= '<b class="text-success">you</b>';
-			
-			
-		}
-	}
-	
-	function get_username($userID, $conn){
-		$query = "SELECT name FROM users
-		WHERE userID = '$userID'";
-		$statement = $conn->prepare($query);
-		$statement->execute();
-		$result = $statement->fetchAll();
-		foreach($result as $row){
-			return $row['name'];
-		}
-	}
-	
+		
 ?>
